3ff8cc2e8583aa8fdaf5bd5ab5fc3483ea5a4167
marcushorne <marc.horne@amorelie.com>
2019-07-15 11:59:50 +0200

    Divided by department

--- src/app/components/employees.component.ts
+++ src/app/components/employees.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Store } from '@ngrx/store';
+import { groupBy } from 'lodash';
 
 @Component({
   selector: 'app-root',
@@ -8,11 +9,29 @@ import { Store } from '@ngrx/store';
 
 export class EmployeeComponent {
   public employees: any;
+  public departments: any;
   public filter: string;
 
   constructor(private store: Store<any>) {
-    store.select('employees').subscribe(employees => this.employees = employees);
+    store.select('employees').subscribe(employees => this.processEmployees(employees));
   }
 
   filterList(value) { this.filter = (value === '') ? null : value; }
+    
+  processEmployees(employees) {
+    const grouped = groupBy(employees, 'department');
+    const management = grouped['Management'];
+    const not_defined = grouped['undefined'];
+    delete grouped['Management'];
+    delete grouped['undefined'];
+    
+    const arrayed = Object.values(grouped);
+    const arrayed_management = Object.values(management);
+    const arrayed_undefined = Object.values(not_defined);
+    
+    arrayed.unshift(arrayed_management)
+    arrayed.push(arrayed_undefined);
+    
+    this.departments = arrayed;
+  }
 }
